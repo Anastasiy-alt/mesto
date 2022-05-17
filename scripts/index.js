a2993b434766955137ecfe612e519a872f9f44a9
Anastasiy-alt <n060600@yandex.ru>
2022-05-17 20:53:21 +0500

    with Toshi

--- scripts/index.js
+++ scripts/index.js
@@ -16,41 +16,42 @@ let linkImage = document.querySelector('[name=link]');
 const blockCards = document.querySelector('.elements');
 const buttonAddImg = document.querySelector('#button-add-img');
 
-
 const initialCards = [
   {
-    name: 'Собака-Чубака',
-    link: 'https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-  },
-  {
-    name: 'Собака-Чубака',
-    link: 'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1498&q=80'
-  },
-  {
-    name: 'Собака-Чубака',
-    link: 'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1317&q=80'
-  },
-  {
-    name: 'Собака-Чубака',
-    link: 'https://images.unsplash.com/photo-1530041539828-114de669390e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80'
-  },
-  {
-    name: 'Собака-Чубака',
+    name: 'Toshi в толстовке',
     link: 'https://images.unsplash.com/photo-1534432586043-ead5b99229fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1508&q=80'
   },
   {
-    name: 'Собака-Чубака',
+    name: 'Toshi в джинсовке',
+    link: 'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1317&q=80'
+  }, 
+  {
+    name: 'Toshi француз',
+    link: 'https://images.unsplash.com/photo-1517519014922-8fc06b814a0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'
+  },
+  {
+    name: 'Toshi в дождивике',
+    link: 'https://images.unsplash.com/photo-1530041686259-53d26f863313?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2619&q=80'
+  },
+  {
+    name: 'Toshi в костюме Тоторо',
     link: 'https://images.unsplash.com/photo-1517423738875-5ce310acd3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1540&q=80'
+  },
+  {
+    name: 'Toshi на стиле',
+    link: 'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
   }
 ];
 
-function addImg(t, l) {
+
+
+function addImg(title, link) {
   const imgTemplate = document.querySelector('#img-template').content;
   const element = imgTemplate.querySelector('.element').cloneNode(true);
 
-  element.querySelector('.element__img').src = l;
-  element.querySelector('.element__name').textContent = t;
-  blockCards.append(element);
+  element.querySelector('.element__img').src = link;
+  element.querySelector('.element__name').textContent = title;
+  blockCards.prepend(element);
 }
 
 function addClick () {
@@ -80,10 +81,20 @@ edit.addEventListener('click', editClick);
 closepopEdit.addEventListener('click', closeClick);
 closepopAdd.addEventListener('click', closeClick);
 add.addEventListener('click', addClick);
+
 buttonAddImg.addEventListener('click', function (evt) {
   evt.preventDefault();
   addImg(titleImage.value, linkImage.value);
   titleImage.value = '';
   linkImage.value = '';
   popupAdd.classList.remove('popup_opened');
+});
+
+initialCards.forEach(function (x) {
+  const imgTemplate = document.querySelector('#img-template').content;
+  const element = imgTemplate.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__name').textContent = x.name;
+  element.querySelector('.element__img').src = x.link;
+  
+  blockCards.append(element);
 });
