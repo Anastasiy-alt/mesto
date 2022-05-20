26d1c47d3000ad0fd33405adda203ba94c5b5e06
Anastasiy-alt <n060600@yandex.ru>
2022-05-20 14:32:24 +0500

    All in 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,131 +1,109 @@
-let edit = document.querySelector('.profile__edit-button');
-// let popup = document.querySelector('.popup');
-let popupEdit = document.querySelector('.popup_for_edit');
-let popupAdd = document.querySelector('.popup_for_add');
-let formEdit = document.querySelector('[name=forma-info]');
-// let formAdd = document.querySelector('[name=forma-add]');
-let nameInput = document.querySelector('[name=name]');
-let infoInput = document.querySelector('[name=info]');
-let naming = document.querySelector('.profile__name');
-let info = document.querySelector('.profile__info');
-let add = document.querySelector('.profile__add-button');
-let closepopAdd = document.querySelector('.popup__close_for_add');
-let closepopEdit = document.querySelector('.popup__close_for_edit');
-let titleImage = document.querySelector('[name=title]');
-let linkImage = document.querySelector('[name=link]');
-let imgOpen = document.querySelector('.img-open');
-let imgBig = document.querySelector('.img-open__img');
-let imgInfo = document.querySelector('.img-open__info');
-let imgClose = document.querySelector('.img-open__close');
+const editBtnProfile = document.querySelector('.profile__edit-button');
+const popupEdit = document.querySelector('.popup_for_edit');
+const popupAdd = document.querySelector('.popup_for_add');
+const formEdit = document.querySelector('[name=forma-info]');
+const nameInput = document.querySelector('[name=name]');
+const infoInput = document.querySelector('[name=info]');
+const profileName = document.querySelector('.profile__name');
+const profileInfo = document.querySelector('.profile__info');
+const addBtnImage = document.querySelector('.profile__add-button');
+const closePopupAdd = document.querySelector('.popup__close_for_add');
+const closepopEdit = document.querySelector('.popup__close_for_edit');
+const titleImage = document.querySelector('[name=title]');
+const linkImage = document.querySelector('[name=link]');
+const imagePopup = document.querySelector('.img-open');
+const imgBig = document.querySelector('.img-open__img');
+const imgInfo = document.querySelector('.img-open__info');
+const imgClose = document.querySelector('.img-open__close');
 const blockCards = document.querySelector('.elements');
 const buttonAddImg = document.querySelector('.popup__button_for_add');
-// const imgTemplate = document.querySelector('.img-template').content;
-// const element = imgTemplate.querySelector('.element').cloneNode(true);
-const initialCards = [
-  {
-    name: 'Toshi в толстовке',
-    link: 'https://images.unsplash.com/photo-1534432586043-ead5b99229fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1508&q=80'
-  },
-  {
-    name: 'Toshi в дождивике',
-    link: 'https://images.unsplash.com/photo-1530041686259-53d26f863313?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2619&q=80'
-  },
-  {
-    name: 'Toshi француз',
-    link: 'https://images.unsplash.com/photo-1517519014922-8fc06b814a0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'
-  },
-  {
-    name: 'Toshi в костюме Тоторо',
-    link: 'https://images.unsplash.com/photo-1517423738875-5ce310acd3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1540&q=80'
-  },
-  {
-    name: 'Toshi в джинсовке',
-    link: 'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1317&q=80'
-  },
-  {
-    name: 'Toshi на стиле',
-    link: 'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-  }
-];
 
-function like (element) { //функция лайка
+//если добавить карточку без картинки, 
+//на её месте появляются лайки при открытии -- это фича, а не баг
+function bindLikeButtonClickHandler (element) { //функция лайка
   element.querySelector('.element__like').addEventListener('click', function (evt) {
     evt.target.classList.toggle('element__like_click');
-    }); //если добавить карточку без картинки, на её месте появляются лайки при открытии -- это фича, а не баг
+    }); 
 }
 
-function deleteCard (element) { //функция удаления карточки
+//функция удаления карточки
+function bindDeleteButtonClickHandler (element) { 
   element.querySelector('.element__delete').addEventListener('click', function (evt) {
     element.remove();
     });
 }
 
-function addClick () { //открытие попапа добавления карточки
-  popupAdd.classList.add('popup_opened');
+//открытие попапа
+function openModalWindow (modalWindow) {
+  modalWindow.classList.add('popup_opened');
+} 
+
+//закрытие попапа
+function closeModalWindow (modalWindow) {
+  modalWindow.classList.remove('popup_opened');
+} 
+
+//функция заполнения инпутов
+function fillingInput (info, name) {
+  info.value = profileInfo.textContent;
+  name.value = profileName.textContent;
 }
 
-function editClick () { //открытие попапа редактирования профиля
-    popupEdit.classList.add('popup_opened');
-    infoInput.value = info.textContent;
-    nameInput.value = naming.textContent;
+//открытие попапа редактирования профиля
+function editClick (modalWindow, ) { 
+  openModalWindow(modalWindow);
+  fillingInput (infoInput, nameInput);
 }
 
-function closeClick () { //закрытие попапа
-  popupEdit.classList.remove('popup_opened');
-  popupAdd.classList.remove('popup_opened');
-  imgOpen.classList.remove('popup_opened');
+//форма отправки имени и инфо
+function handlerSubmitEditForm (evt) { 
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileInfo.textContent = infoInput.value;
+  closeModalWindow (popupEdit);
 }
 
-function formSubmitHandler (evt) { //форма отправки имени и инфо
-    evt.preventDefault();
-    naming.textContent = nameInput.value;
-    info.textContent = infoInput.value;
-    popupEdit.classList.remove('popup_opened');
-}
-
-function openImgFunction (evt) { //функция открытия картинки
-  imgOpen.classList.add('popup_opened');
-  evt.target.classList.toggle('element__like_click');
+//функция открытия картинки
+function handlerOpenImgFunction (evt) { 
+  openModalWindow (imagePopup);
+  evt.target.classList.toggle('element__like_click'); //фича
   imgBig.src = evt.target.src;
   imgInfo.textContent = evt.target.alt;
   imgBig.alt = evt.target.alt;
 };
 
-function addImg(title, link) { //функция добавления картинки
+//функция поиска картинки и её дабавления
+function searchImg (title, link) {
   const imgTemplate = document.querySelector('.img-template').content;
-  const element = imgTemplate.querySelector('.element').cloneNode(true);
-  element.querySelector('.element__img').src = link;
-  element.querySelector('.element__name').textContent = title;
-  element.querySelector('.element__img').alt = title;
-  like (element);
-  deleteCard (element) 
-  element.querySelector('.element__img').addEventListener('click', openImgFunction);
-  blockCards.prepend(element);
-};
+  const card = imgTemplate.querySelector('.element').cloneNode(true);
+  card.querySelector('.element__img').src = link;
+  card.querySelector('.element__name').textContent = title;
+  card.querySelector('.element__img').alt = title;
+  bindLikeButtonClickHandler (card);
+  bindDeleteButtonClickHandler (card);
+  card.querySelector('.element__img').addEventListener('click', handlerOpenImgFunction);
+  blockCards.prepend(card);
+}
 
-formEdit.addEventListener('submit', formSubmitHandler);
-edit.addEventListener('click', editClick);
-closepopEdit.addEventListener('click', closeClick);
-closepopAdd.addEventListener('click', closeClick);
-add.addEventListener('click', addClick);
-imgClose.addEventListener('click', closeClick);
-
-initialCards.forEach(function (x) { //авто загрузка карточек
-  const imgTemplate = document.querySelector('.img-template').content;
-  const element = imgTemplate.querySelector('.element').cloneNode(true);
-  element.querySelector('.element__name').textContent = x.name;
-  element.querySelector('.element__img').src = x.link;
-  element.querySelector('.element__img').alt = x.name;
-  like (element);
-  deleteCard (element);
-  element.querySelector('.element__img').addEventListener('click', openImgFunction);
-  blockCards.append(element);
+//авто загрузка карточек
+initialCards.forEach(function (x) { 
+  imgName = x.name;
+  imgLink = x.link;
+  searchImg (imgName, imgLink);
 });
 
-buttonAddImg.addEventListener('click', function (evt) { //вызов функции добавления картинки через кнопку add
+//вызов функции добавления картинки через кнопку add
+buttonAddImg.addEventListener('click', function (evt) { 
   evt.preventDefault();
-  addImg(titleImage.value, linkImage.value);
+  searchImg (titleImage.value, linkImage.value);
   titleImage.value = '';
   linkImage.value = '';
-  popupAdd.classList.remove('popup_opened');
+  closeModalWindow (popupAdd);
 });
+
+addBtnImage.addEventListener('click', () => { openModalWindow (popupAdd); });
+editBtnProfile.addEventListener('click', () => { editClick (popupEdit); });
+closepopEdit.addEventListener('click', () => { closeModalWindow (popupEdit); });
+closePopupAdd.addEventListener('click', () => { closeModalWindow (popupAdd); });
+imgClose.addEventListener('click', () => { closeModalWindow (imagePopup); });
+formEdit.addEventListener('submit', handlerSubmitEditForm);
